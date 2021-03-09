fa0dff9448f4ef762ae15b27bf991e66cd515283
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-03-09 00:08:11 -0300

    Update name-search.php

--- connections/name-search.php
+++ connections/name-search.php
@@ -1,6 +1,6 @@
 <?php
-    if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
-        if (isset($_GET['name'])) {
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')){
+        if(isset($_GET['name'])){
             $curl = curl_init();
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
                     $name_to_search = htmlentities(strtolower($_GET['name'])); // HuLk == hulk
